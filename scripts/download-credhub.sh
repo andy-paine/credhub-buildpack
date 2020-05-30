e6e3998199fd43d813fc3041eef6d8d5c0991314
Andy Paine <andy.paine@engineerbetter.com>
2020-05-30 22:28:36 +0100

    Use buildpack-packager's built-in dependencies
    No need to write a custom script for downloading the credhub CLI since
    it can be included in the dependencies block of the manifest.yml

--- scripts/download-credhub.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-version="$(cat CREDHUB_VERSION)"
-curl -sLo credhub.tgz "https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/$version/credhub-linux-$version.tgz"
-tar -xvf credhub.tgz -C bin
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 go get -u github.com/cloudfoundry/libbuildpack/packager/buildpack-packager
-buildpack-packager build -stack cflinuxfs3
+buildpack-packager build -stack cflinuxfs3 -cached
